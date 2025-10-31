ecc1cc1c76555e31c939d823ff9e3a5068b41639
Eric Alfaro <ealfaro@mit.edu>
2025-10-31 19:56:41 -0400

    Marcher refactoring

--- rendering/rendering_plane.gd
+++ rendering/rendering_plane.gd
@@ -4,7 +4,13 @@ class_name RenderingPlane extends MeshInstance3D
 @export var camera: Camera3D 
 
 func _process(_delta: float) -> void:
-	set_instance_shader_parameter("_frac_pos", global_position)
-	material_override.set_shader_parameter("_fov", camera.fov)
-	material_override.set_shader_parameter("_cam_mat", camera.global_transform.basis)
-	material_override.set_shader_parameter("_aspect_ratio", float(get_window().size.x) / get_window().size.y)
+	set_instance_shader_parameter("_position", global_position)
+	set_instance_shader_parameter("_scale", scale.x) # Assumes uniform scaling
+	set_instance_shader_parameter("_rotation", rotation)
+	
+	var target_camera: Camera3D = camera
+	if not is_instance_valid(target_camera):
+		target_camera = EditorInterface.get_editor_viewport_3d(0).get_camera_3d()
+	
+	material_override.set_shader_parameter("_fov", target_camera.fov)
+	material_override.set_shader_parameter("_cam_mat", target_camera.global_transform.basis)
