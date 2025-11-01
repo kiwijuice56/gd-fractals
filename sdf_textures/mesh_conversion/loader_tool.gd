3aabf7558283a5ca498bedbee43a04c26245e688
Eric Alfaro <ealfaro@mit.edu>
2025-10-31 21:57:47 -0400

    More tweaks to SDF renderer

--- sdf_textures/mesh_conversion/loader_tool.gd
+++ sdf_textures/mesh_conversion/loader_tool.gd
@@ -3,5 +3,5 @@ class_name LoaderTool extends EditorScript
 # Tool script to use SDFLoader within the editor
 
 func _run() -> void:
-	var texture: ImageTexture3D = SDFLoader.load_sdf_texture("res://sdf_textures/mesh_conversion/output/teto.sdf", 64)
-	ResourceSaver.save(texture, "res://sdf_textures/teto.res")
+	var texture: ImageTexture3D = SDFLoader.load_sdf_texture("res://sdf_textures/mesh_conversion/output/teto_pear.sdf", 66)
+	ResourceSaver.save(texture, "res://sdf_textures/teto_pear.res")
--- sdf_textures/mesh_conversion/obj_to_sdf.py
+++ sdf_textures/mesh_conversion/obj_to_sdf.py
@@ -8,8 +8,6 @@ def normalize_sdf(sdf_array):
     min_val = sdf_array.min()
     max_val = sdf_array.max()
 
-    print("min: ", min_val, " max: ", max_val)
-
     # Normalize to 0-1 range
     normalized = (sdf_array - min_val) / (max_val - min_val)
 
@@ -24,8 +22,9 @@ def save_as_raw(sdf_array, output_path):
     with open(output_path, 'wb') as f:
         f.write(texture_data.tobytes())
 
-mesh = trimesh.load('input/teto.obj')
-voxels = mesh_to_voxels(mesh, voxel_resolution=64, surface_point_method='scan', sign_method='normal',
-                        scan_count=100, scan_resolution=400, sample_point_count=100000,
+N = 64
+mesh = trimesh.load('input/teto_pear.obj')
+voxels = mesh_to_voxels(mesh, voxel_resolution=N, surface_point_method='scan', sign_method='normal',
+                        scan_count=100, scan_resolution=400, sample_point_count=10000000,
                         normal_sample_count=11, pad=False, check_result=False, return_gradients=False)
-save_as_raw(voxels, 'output/teto.sdf')
+save_as_raw(voxels, 'output/teto_pear.sdf')
